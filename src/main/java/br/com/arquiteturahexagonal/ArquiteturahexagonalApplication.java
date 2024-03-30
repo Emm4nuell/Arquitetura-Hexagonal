dcafc7006c5ce3007aab2c256263bf96b83a8dd1
Emm4nuell <eduardo.eesf@gmail.com>
2024-03-30 00:02:17 -0300

    Criado projeto Arquitetura Hexagonal

---
+++ src/main/java/br/com/arquiteturahexagonal/ArquiteturahexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.arquiteturahexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturahexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturahexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arquiteturahexagonal/ArquiteturahexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arquiteturahexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturahexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
