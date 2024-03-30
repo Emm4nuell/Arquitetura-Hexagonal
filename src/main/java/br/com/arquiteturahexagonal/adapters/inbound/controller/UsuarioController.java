150d6af1419d7348851ad544c865d1ac6c366b23
Emm4nuell <eduardo.eesf@gmail.com>
2024-03-30 00:32:45 -0300

    Criado a interface USUARIOSERVICEPORT no application port

--- src/main/java/br/com/arquiteturahexagonal/adapters/inbound/controller/UsuarioController.java
+++ src/main/java/br/com/arquiteturahexagonal/adapters/inbound/controller/UsuarioController.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("hexagonal")
 public class UsuarioController {
-    
+
 }
---
+++ src/main/java/br/com/arquiteturahexagonal/application/port/in/UsuarioServicePort.java
@@ -0,0 +1,8 @@
+package br.com.arquiteturahexagonal.application.port.in;
+
+import br.com.arquiteturahexagonal.application.core.domain.Usuario;
+
+public interface UsuarioServicePort {
+
+    Usuario servicesalvar(Usuario usuario);
+}
