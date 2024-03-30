229d704dfeface1b80d71e729e4dfcae356541f4
Emm4nuell <eduardo.eesf@gmail.com>
2024-03-30 00:30:12 -0300

    Criado o USUARIOCONTROLLER e USUARIOENTITY no adapters

---
+++ src/main/java/br/com/arquiteturahexagonal/adapters/inbound/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+package br.com.arquiteturahexagonal.adapters.inbound.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hexagonal")
+public class UsuarioController {
+    
+}
--- src/main/java/br/com/arquiteturahexagonal/adapters/inbound/entity/UsuarioEntity.java
+++ src/main/java/br/com/arquiteturahexagonal/adapters/inbound/entity/UsuarioEntity.java
@@ -32,6 +32,6 @@ public class UsuarioEntity {
     private String senha;
     private String foto;
     private boolean status;
-    
+
 }
 
